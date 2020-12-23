a358a215ed9c2bfb9b6e7f70b1b6f4e21f7a9f6a
Th3Wall <mandellidavide.95@gmail.com>
2020-12-23 01:04:37 +0100

    added play functionality to the drumpad

--- src/components/Pads/pads.jsx
+++ src/components/Pads/pads.jsx
@@ -1,44 +1,89 @@
 import "./pads.sass";
+import { useState, useEffect, useRef } from 'react'
 
 const Pads = () => {
+
+    const soundBank = [
+        {
+            keyTrigger: 'Q',
+            id: 'Heater-1',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+        },
+        {
+            keyTrigger: 'W',
+            id: 'Heater-2',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+        },
+        {
+            keyTrigger: 'E',
+            id: 'Heater-3',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+        },
+        {
+            keyTrigger: 'A',
+            id: 'Heater-4',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+        },
+        {
+            keyTrigger: 'S',
+            id: 'Clap',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+        },
+        {
+            keyTrigger: 'D',
+            id: 'Open-HH',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+        },
+        {
+            keyTrigger: 'Z',
+            id: "Kick-n'-Hat",
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+        },
+        {
+            keyTrigger: 'X',
+            id: 'Kick',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+        },
+        {
+            keyTrigger: 'C',
+            id: 'Closed-HH',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+        }
+    ];
+
+    const [isPlaying, setIsPlaying] = useState(false);
+    const audioRef = useRef(null);
+
+    const handlePad = () => {
+        const audio = audioRef.current;
+        audio.currentTime = 0;
+        audio.play();
+        setIsPlaying(true)
+    }
+
+    useEffect(() => {
+        audioRef.current.addEventListener('ended', () => {
+            setIsPlaying(false)
+        })
+    }, [])
+
     return (
         <div className='pads'>
-            <div className="drum-pad">
-                <audio src="" id='Q' className='clip'></audio>
-                <span>Q</span>
-            </div>
-            <div className="drum-pad">
-                <audio src="" id='W' className='clip'></audio>
-                <span>W</span>
-            </div>
-            <div className="drum-pad">
-                <audio src="" id='E' className='clip'></audio>
-                <span>E</span>
-            </div>
-            <div className="drum-pad">
-                <audio src="" id='A' className='clip'></audio>
-                <span>A</span>
-            </div>
-            <div className="drum-pad">
-                <audio src="" id='S' className='clip'></audio>
-                <span>S</span>
-            </div>
-            <div className="drum-pad">
-                <audio src="" id='D' className='clip'></audio>
-                <span>D</span>
-            </div>
-            <div className="drum-pad">
-                <audio src="" id='Z' className='clip'></audio>
-                <span>Z</span>
-            </div>
-            <div className="drum-pad">
-                <audio src="" id='X' className='clip'></audio>
-                <span>X</span>
-            </div>
-            <div className="drum-pad">
-                <audio src="" id='C' className='clip'></audio>
-                <span>C</span>
-            </div>
+            {soundBank && soundBank.map((pad, idx) => (
+                <div
+                    className="drum-pad"
+                    key={idx}
+                    onClick={!isPlaying && handlePad}
+                >
+                    <audio
+                        src={pad.url}
+                        id={pad.keyTrigger}
+                        ref={audioRef}
+                        className='clip'
+                    ></audio>
+                    <span>{pad.keyTrigger}</span>
+                </div>
+            ))}
         </div>
     )
 }
